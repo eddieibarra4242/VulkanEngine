f5ca983e77a8c8f7c113e8f843ad1910c4353bf7
Eduardo Ibarra <eduardoibarra4242@gmail.com>
2022-07-07 14:47:49 -0700

    Added Buffers

---
+++ Engine/rendering/Buffer.cpp
@@ -0,0 +1,132 @@
+#include "Buffer.hpp"
+
+#include "VulkanUtils.hpp"
+
+#include <memory.h>
+
+void createBuffer(const Device& device, VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory)
+{
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = size;
+    bufferInfo.usage = usage;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    ASSERT_VK_SUCCESS(vkCreateBuffer(device.device(), &bufferInfo, nullptr, &buffer), "failed to create buffer!");
+
+    VkMemoryRequirements memRequirements;
+    vkGetBufferMemoryRequirements(device.device(), buffer, &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex = device.findMemoryType(memRequirements.memoryTypeBits, properties);
+
+    ASSERT_VK_SUCCESS(vkAllocateMemory(device.device(), &allocInfo, nullptr, &bufferMemory), "failed to allocate buffer memory!");
+
+    vkBindBufferMemory(device.device(), buffer, bufferMemory, 0);
+}
+
+Buffer::Buffer(const Device& device, VkDeviceSize instanceSize, VkDeviceSize instanceCount, VkDeviceSize alignment, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties) : m_instanceCount{ instanceCount }, m_usage{ usage }, m_properties{ properties }, m_parent_dev{ device }
+{
+    m_alignedInstanceSize = align(instanceSize, alignment);
+    m_size = m_alignedInstanceSize * m_instanceCount;
+
+    m_bufInfo.resize(m_instanceCount);
+
+    createBuffer(device, m_size, m_usage, m_properties, m_buffer, m_buf_mem);
+
+    if ((m_properties & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) == VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+        vkMapMemory(device.device(), m_buf_mem, 0, m_size, 0, &m_pMap);
+    }
+}
+
+Buffer::~Buffer()
+{
+    destroy_internal();
+}
+
+void Buffer::destroy_internal()
+{
+    if (m_pMap != nullptr) {
+        vkUnmapMemory(m_parent_dev.device(), m_buf_mem);
+    }
+
+    if (m_buffer != VK_NULL_HANDLE) {
+        vkDestroyBuffer(m_parent_dev.device(), m_buffer, nullptr);
+        vkFreeMemory(m_parent_dev.device(), m_buf_mem, nullptr);
+    }
+}
+
+void Buffer::operator=(Buffer&& other)
+{
+    destroy_internal();
+
+    this->m_buffer = other.m_buffer;
+    this->m_buf_mem = other.m_buf_mem;
+
+    this->m_size = other.m_size;
+    this->m_alignedInstanceSize = other.m_alignedInstanceSize;
+    this->m_instanceCount = other.m_instanceCount;
+
+    this->m_usage = other.m_usage;
+    this->m_properties = other.m_properties;
+
+    this->m_pMap = other.m_pMap;
+
+    other.m_buffer = VK_NULL_HANDLE;
+    other.m_buf_mem = VK_NULL_HANDLE;
+
+    other.m_pMap = nullptr;
+}
+
+void Buffer::write(const void* data, size_t size, VkDeviceSize index, size_t offsetInIndex)
+{
+    if (size == 0) {
+        return;
+    }
+
+    if (m_pMap != nullptr) {
+        VkDeviceSize indexOffset = index * m_alignedInstanceSize;
+        char* dest = reinterpret_cast<char*>(m_pMap) + indexOffset + offsetInIndex;
+        memcpy(dest, data, size);
+
+        if ((m_properties & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) != VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) {
+            // FIXME: use nonCoherentAtomSize when calculating offset and size here.
+
+            // flush the whole index
+            VkMappedMemoryRange mappedRange = {};
+            mappedRange.sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+            mappedRange.memory = m_buf_mem;
+            mappedRange.offset = indexOffset;
+            mappedRange.size = m_alignedInstanceSize;
+            vkFlushMappedMemoryRanges(m_parent_dev.device(), 1, &mappedRange);
+        }
+
+        return;
+    }
+
+    VkBuffer staging;
+    VkDeviceMemory smem;
+    createBuffer(m_parent_dev, size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, staging, smem);
+
+    void* staging_dest;
+    vkMapMemory(m_parent_dev.device(), smem, 0, size, 0, &staging_dest);
+    memcpy(staging_dest, data, size);
+    vkUnmapMemory(m_parent_dev.device(), smem);
+
+    size = std::min(size, m_size);
+
+    VkCommandBuffer commandBuffer = beginSingleTimeCommands(m_parent_dev.device(), m_parent_dev.graphicsPool());
+
+    VkBufferCopy copyRegion{};
+    copyRegion.srcOffset = 0;
+    copyRegion.dstOffset = index * m_alignedInstanceSize;
+    copyRegion.size = size;
+    vkCmdCopyBuffer(commandBuffer, staging, m_buffer, 1, &copyRegion);
+
+    endSingleTimeCommands(m_parent_dev.device(), m_parent_dev.graphicsQueue(), m_parent_dev.graphicsPool(), commandBuffer);
+
+    vkDestroyBuffer(m_parent_dev.device(), staging, nullptr);
+    vkFreeMemory(m_parent_dev.device(), smem, nullptr);
+}
---
+++ Engine/rendering/Buffer.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+#include <algorithm>
+
+#include "Device.hpp"
+
+class Buffer
+{
+  public:
+    [[nodiscard]] static constexpr VkDeviceSize align(size_t size, VkDeviceSize alignment)
+    {
+        return (static_cast<VkDeviceSize>(size) + alignment - 1) & ~(alignment - 1);
+    }
+
+    Buffer(const Device& device, VkDeviceSize instanceSize, VkDeviceSize instanceCount, VkDeviceSize alignment, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties);
+    ~Buffer();
+
+    [[nodiscard]] constexpr const void* data() const { return m_pMap; }
+    [[nodiscard]] constexpr void* data() { return m_pMap; }
+
+    [[nodiscard]] constexpr const VkBuffer& buffer() const { return m_buffer; }
+
+    [[nodiscard]] constexpr VkDeviceSize size() const { return m_size; }
+    [[nodiscard]] constexpr VkDeviceSize instanceSize() const { return m_alignedInstanceSize; }
+    [[nodiscard]] constexpr VkDeviceSize instanceCount() const { return m_instanceCount; }
+
+    [[nodiscard]] inline VkDescriptorBufferInfo& descInfo(uint32_t index)
+    {
+        m_bufInfo[index].buffer = m_buffer;
+        m_bufInfo[index].offset = index * m_alignedInstanceSize;
+        m_bufInfo[index].range = m_alignedInstanceSize;
+
+        return m_bufInfo[index];
+    }
+
+    void write(const void* data, size_t size, VkDeviceSize index, size_t offsetInIndex = 0);
+
+    // FIXME: We can copy here.
+    DELETE_COPY(Buffer);
+
+    void operator=(Buffer&& other);
+    inline Buffer(Buffer&& other) : m_buffer{ VK_NULL_HANDLE }, m_buf_mem{ VK_NULL_HANDLE }, m_pMap{ nullptr }, m_parent_dev(other.m_parent_dev) { this->operator=(std::move(other)); }
+
+  private:
+    VkBuffer m_buffer;
+    VkDeviceMemory m_buf_mem;
+
+    VkDeviceSize m_size;
+    VkDeviceSize m_alignedInstanceSize;
+    VkDeviceSize m_instanceCount;
+
+    VkBufferUsageFlags m_usage;
+    VkMemoryPropertyFlags m_properties;
+
+    std::vector<VkDescriptorBufferInfo> m_bufInfo;
+
+    void* m_pMap = nullptr;
+
+    const Device& m_parent_dev;
+
+    void create_internal(const Buffer* other, const Device& device, VkDeviceSize instanceSize, VkDeviceSize instanceCount, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties);
+    void destroy_internal();
+};
